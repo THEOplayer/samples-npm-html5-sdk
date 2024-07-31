0b740f33254b1537f2102804d210e0a5ff63801f
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-07-31 16:04:47 +0200

    Add Vite samples

---
+++ samples/vite/javascript/README.md
@@ -0,0 +1,11 @@
+# vite-javascript
+
+This sample project demonstrates the usage of the THEOplayer HTML5 Video Player SDK in Vite using NPM and JavaScript.
+
+## Usage
+
+Refer to the top-level [README.md](../../README.md).
+
+## Frequently Asked Questions
+
+Refer to the top-level [README.md](../../README.md).
---
+++ samples/vite/typescript/src/index.ts
@@ -0,0 +1,14 @@
+import * as THEOplayer from 'theoplayer';
+import {LICENSE} from '../../../util/constants';
+import {Player, PlayerConfiguration} from 'theoplayer';
+
+const element: HTMLElement = document.getElementById('my-player')!;
+const configuration: PlayerConfiguration = {
+    license: LICENSE,
+    libraryLocation: '/vendor/theoplayer/'
+};
+
+const player: Player = new THEOplayer.Player(element, configuration);
+
+player.src = '//cdn.theoplayer.com/video/elephants-dream/playlist.m3u8';
+
